1503069be685628b3eec7a94664ddc9f0e47348a
Verdauen.com <khalis411@gmail.com>
2018-07-06 18:27:00 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+include "api.php";
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *'); 
+if (!empty($_GET['id']) && isset($_GET['id'])) {
+// $udkux = $_SERVER['HTTP_REFERER'];
+// if (strpos($udkux, 'joneskun.com') !== false) {
+// } else {
+// }
+
+$getGP = getPhotoGoogle($_GET['id']);
+echo $getGP;
+} else {
+    exit('No Id provided!');
+}
+?>
